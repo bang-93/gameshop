e09b5b948bdab64f252140c93c797856a20f1aa9
tjral <tjral@1.237.99.249>
2021-07-28 21:23:38 +0900

    2021-07-28
    판매상품 리스트 추가완료
    
    smbang

---
+++ src/main/java/co/kr/controller/SaleProdController.java
@@ -0,0 +1,16 @@
+package co.kr.controller;
+
+import org.springframework.stereotype.Controller;
+
+/*
+ * 판매상품 컨트롤러
+ * -작업자이름 
+ * -version 1.0.0
+ * */
+
+@Controller
+public class SaleProdController {
+	
+	//팀원중 상품을 맞는사람이 작업했습니다.
+
+}
