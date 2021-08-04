72776e800935c7f372ed0d41885ded4d49feb33c
yeomteaho <iooixogh@gmail.com>
2021-08-05 08:18:10 +0900

    장바구닌 임시파일

--- src/main/java/kr/co/gameshop/controller/client/CartController.java
+++ src/main/java/kr/co/gameshop/controller/client/CartController.java
@@ -4,77 +4,58 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.inject.Inject;
 import javax.servlet.http.HttpSession;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import kr.co.gameshop.vo.CartVO;
+/**import ko.co.gameshop.dto.CartDTO;
 
-@Controller
-@RequestMapping("/gameshop/cart/")
 public class CartController {
-/**
-	@Inject
-	CartService CartService;
-	
-	//1. 장바구니 추가
-	@RequestMapping("insert.do")
-	public String insert(@ModelAttribute VartVO vo, HttpSession session) {
-		String userId = (String) session.getAttribute("userId");
-		vo.SetUserId(userId);
-		//장바구니에 기존 상품이 있는지 확인검사
-		int count = cartService.countCart(vo.getProductId(), userId);
-		count == 0 ? cartService.updateCart(vo) : cartService.insert(vo);
-		if(count == 0) {
-			//없으면 생성
-			cartService.insert(vo);
-		}else {
-			//있으면 등록
-			cartService.updateCart(vo);
-		}
-		return "redirect:/gameshop/cart/list.do";		
-	}
-	
-	//장바구니 목록
+
 	@RequestMapping("list.do")
-	public ModelAndView list(HttpSession session, ModelAndView mav){
-		String userId = (String) session.getAttribute("userId");
-		Map<String, Object> map = new HashMap<String, Object>();
-		List<CartVO> list = cartService.listCart(userId);//장바구니 정보
-		int sumMoney = cartService.sumMoney(userId); //자바구니 전체 금액 호출
-		map.put("list", list);					//장바구니 정보를 map에 저장
-		map.put(count, list.size());			//장바구니 상품의 유무
-		map.put("sumMoney", sumMoney);			//장바구니 전체금액
-		map.put("allSum", sumMoney);			//주문 상품 전체금액
-		mav.setViewName("gameshop/cartList");	//view(jsp)의 이름 저장
-		mav.addObject("map",map);				//map변수 저장
-		return mav;		
-	}
-	
-	//장바구니 삭제
-	@RequestMapping("delete.do")
-	public String delete(@RequestParam int cartId) {
-		cartService.delete(cartId);
-		return "redirect:/gameshop/cart/list.do;
-	}
-	
-	//장바구니 목록
-	@RequestMapping("list.do")
-	public String update(@RequestParam int[] amount, @RequestParam int[] productId, HttpSession session) {
-		String userId = (String) session.getAttribute("userId");
-		//레코드의 갯수 만큼 반복문 실행
-		for(int i=0; i<productId.length;i++) {
-			CartVO vo = new CartVO();
-			vo.setUserId(userId);
-			vo.setAmount(amount[i]);
-			vo.setProductId(productId[i]);
-			cartService.modifyCart(vo);
-		}
-		return"redirect:/gameshop/cart/list.do";
-	}
-**/}
+    public ModelAndView list(HttpSession session, ModelAndView mav) {
+        Map<String, Object> map=new HashMap<>();
+
+        //hashmap은 map(key,value)로 이뤄져 있고,
+        //key값은 중복이 불가능 하고 value는 중복이 가능하다.
+        //value에 null값도 사용이 가능하다.
+        //전달할 정보가 많을 경우에는 HashMap<>을 사용하는 것이 좋다.
+        //장바구니에 담을 값들이 많기 때문에 여기선 HashMap<>를 사용한다.
+                
+
+        String userid=(String)session.getAttribute("userid");
+
+        //session에 저장된 userid를 getAttribute()메소드를 사용해서 얻어오고 오브젝트 타입이기 때문에
+        //String 타입으로 타입변환한다.
+
+        if(userid!=null) { 
+            //로그인한 상태이면 실행
+            List<CartDTO> list=cartService.listCart(userid);//장바구니 목록
+            int sumMoney=cartService.sumMoney(userid);//금액 합계
+            int fee=sumMoney >= 30000 ? 0 : 2500; 
+            
+            //배송료 계산
+            //30000원이 넘으면 배송료가 0원, 안넘으면 2500원
+            
+           //hasp map에 장바구니에 넣을 각종 값들을 저장함
+            map.put("sumMoney", sumMoney);
+            map.put("fee", fee); //배송료
+            map.put("sum", sumMoney+fee); //전체 금액
+            map.put("list", list); //장바구니 목록
+            map.put("count", list.size()); //레코드 갯수
+
+           //ModelAndView mav에 이동할 페이지의 이름과 데이터를 저장한다.
+
+            mav.setViewName("shop/cart_list"); //이동할 페이지의 이름
+            mav.addObject("map", map); //데이터 저장
+
+            return mav; //화면 이동
+
+        }else { //로그인하지 않은 상태
+
+            return new ModelAndView("member/login", "", null);
+            //로그인을 하지 않았으면 로그인 페이지로 이동시킨다.
+        }
+    }**/
--- src/main/java/kr/co/gameshop/dao/CartDAO.java
+++ src/main/java/kr/co/gameshop/dao/CartDAO.java
@@ -1,53 +1,19 @@
 package kr.co.gameshop.dao;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import javax.inject.Inject;
+/**import ko.co.gameshop.dto.CartDTO;
 
-import org.apache.ibatis.session.SqlSession;
-import org.springframework.stereotype.Repository;
-
-import kr.co.gameshop.vo.CartVO;
-
-/**@Repository
-public class CartDAO {
-	@Inject
-	SqlSession sqlSession;
-	
-	//장바구니 추가
-	public void insert(CartVO vo) {
-		sqlSession.insert("cart.insertCart",vo);
-	}
-	//장바구니 목록
-	public List<CartVO> listCart(String userId){
-		return sqlSession.selectList("cart.listCart", userId);
-	}
-	//장바구니 삭제
-	public void delete(int cartId) {
-		sqlSession.delete("cart.deleteCart", cartId);
-	}
-	//장바구니 수정
-	public void modifyCart(CartVO vo) {
-		sqlSession.update("cart.modifyCart",vo);
-	}
-	//장바구니 금액 합계
-	public int sumMoney(String userId) {
-		sqlSession.selectOne("cart.sumMoney", userId);
-		return sqlSession.selectOne("cart.sumMoney", userId);
-	}
-	//장바구니 동일한 상품 레코드 확인
-	public int countCart(int productId, String userId) {
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("productId", productId);
-		map.put("userId", userId);
-		return sqlSession.selectOne("cart.countCart", map);
-	}
-	
-	//장바구니 수량 변경
-	public void updateCart(CartVO,vo) {
-		sqlSession.update("cart.sumCart",vo);
-	}
-
+public interface CartDAO {
+    List<CartDTO> cartMoney();
+    void insert(CartDTO dto); //장바구니 추가
+    List<CartDTO> listCart(String userid); //장바구니 목록
+    void delete(int cart_id); //장바구니 개별 삭제
+    void deleteAll(String userid); //장바구니 비우기
+    void update(int cart_id); 
+    int sumMoney(String userid); //장바구니 금액 합계
+    int countCart(String userid, int product_id); //장바구니 상품 갯수
+    void updateCart(CartDTO dto); //장바구니 수정 
+    void modifyCart(CartDTO dto);
 }**/
+
